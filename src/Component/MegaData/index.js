5b6b13d62c9b9d5466d2c24f36ee18be8729b8fa
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-01-16 17:27:02 +0500

    4677

--- src/Component/MegaData/index.js
+++ src/Component/MegaData/index.js
@@ -68,12 +68,12 @@ const MegaData = ({ categoryId }) => {
           onClick={() => handleServiceClick(service.id)}
           className="flex flex-col sm:flex-row border md:border-none mt-2 md:mt-0 mb-3 md:mb-0 duration-300 transition gap-3 rounded-md hover:bg-primary-blue100 hover:text-white px-2 py-2 cursor-pointer"
         >
-          <Image
+          <img
             className="w-[30px] h-[30px] object-cover bg-center rounded-full"
             width={200}
             height={200}
-            src={`${service.serviceImage.replace("public/", "")}`}
-            alt={`${service.serviceImage.replace("public/", "")}`}
+            src={`/${service.serviceImage.replace("public/", "")}`}
+            alt={`/${service.serviceImage.replace("public/", "")}`}
           />
           <div>
             <HeadingH6 initial={{ opacity: 1, x: 0 }}
