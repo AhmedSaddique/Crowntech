05cfb3f6200e73ae48723d0f55e31c0b3b991a1e
AhmedSaddique <ahmed.saddique12@gmail.com>
2024-02-03 17:27:45 +0500

    asd

--- src/app/api/database.js
+++ src/app/api/database.js
@@ -1,25 +1,12 @@
-// pages/api/database.js
+// db.js
+import { Pool } from "pg";
 
-import { Pool } from 'pg';
-
-// Create a PostgreSQL connection pool
 const pool = new Pool({
-  user: 'postgres',
-  host: 'PostgreSQL',
-  database: 'crown',
+  user: 'crown',
+  host: 'localhost',
+  database: 'test',
   password: 'crown',
   port: 5432, // Default PostgreSQL port
 });
 
-export default async function handler(req, res) {
-  try {
-    // Example query
-    const result = await pool.query('SELECT * FROM crowntech');
-    res.status(200).json({ success: true, data: result.rows });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, error: error.message });
-  } finally {
-    pool.end(); // Close the connection pool
-  }
-}
+export { pool }; // Use curly braces for named export
